26b21bc69612178f84dfae778308f7fdad5c41f8
igorskeff <igorskeff@users.noreply.github.com>
2024-05-24 19:58:43 -0300

    Adição das funções de TED
    Adição das seguintes funções:
    -consulta_info_conta
    -transferir_ted

---
+++ scripts/ted.c
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 24/05/2024.
+//
+
+#include "ted.h"
---
+++ scripts/ted.h
@@ -0,0 +1,8 @@
+//
+// Created by Usuario on 24/05/2024.
+//
+
+#ifndef APLICACAO_BACK_END_TED_H
+#define APLICACAO_BACK_END_TED_H
+
+#endif //APLICACAO_BACK_END_TED_H
