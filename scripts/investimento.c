8322642367d625be32a34d0b6a51a7a7d571e404
igorskeff <igorskeff@users.noreply.github.com>
2024-05-31 17:58:41 -0300

    Ajuste no investimento

--- scripts/investimento.c
+++ scripts/investimento.c
@@ -4,8 +4,16 @@
 #include <time.h>
 #include <string.h>
 #include "transferencia.h"
+
 char * investimento(char * post) {
-    char * data = pegaData();
+    time_t agora;
+    time(&agora);
+
+    struct tm * data_hora = localtime(&agora);
+
+    char data[11];
+    sprintf(data, "%02d/%02d/%04d", data_hora->tm_mday, data_hora->tm_mon + 1, data_hora->tm_year + 1900);
+
     ListCampo post_data = convertObj(post);
     char *query = "INSERT INTO TAB_INVESTIMENTO( ";
     for (int i = 0; i < post_data.tamanho; i++) {
@@ -26,12 +34,12 @@ char * investimento(char * post) {
     query = concatena(query, "\'");
     query = concatena(query, ")");
     Linhas retorno = bd(query);
-    char * json = "{\"USU_ID_ORIGEM\":\"";
+    char * json = "{USU_ID_ORIGEM:";
     json = concatena(json, post_data.campos[0].valor);
-    json = concatena(json, "\",\"USU_ID_DESTINO\":\"1\",\"VALOR\":\"");
+    json = concatena(json, ",USU_ID_DESTINO:1,VALOR:");
     json = concatena(json,post_data.campos[1].valor);
-    json = concatena(json,"\",\"TIPO\":\"INVESTIMENTO\"}");
-    transferir(json);
+    json = concatena(json,",TIPO:INVESTIMENTO}");
+    char * RetornoJSON = transferir(json);
     return "{\"mensagem\":\"aplicado\"}";
 }
 char * mostrarInvestimento(char * post)
