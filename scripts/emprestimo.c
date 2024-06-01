421ed0844b02772172acfbeb586f1e4246100c3d
Leonardo Modesto <leonardoaparecido216@gmail.com>
2024-06-01 16:03:39 -0300

    update no emprestimo

--- scripts/emprestimo.c
+++ scripts/emprestimo.c
@@ -55,48 +55,14 @@ char * lista_emprestimo(char * post)
 
     ListCampo post_data = convertObj(post);
 
-    char * query = "SELECT "
-                   "USU_ID,"
-                   "EMP_VALOR_TOTAL,"
-                   "EMP_VALOR_PAGO,"
-                   "EMP_NUM_PARCELAS,"
-                   "EMP_JURUS_MENSAL,"
-                   "EMP_DATA_INICIO,"
-                   "EMP_COBRADO "
-                   "FROM "
-                   "TAB_EMPRESTIMO "
-                   "WHERE "
-                   "(USU_ID = ";
+    char * query = "SELECT * FROM TAB_USUARIO WHERE USU_ID =";
+
     query  = concatena(query,post_data.campos[0].valor);
-    query = concatena(query,")");
     Linhas retorno = bd(query);
     if(retorno.tamanho > 0)
     {
-        char * json = "[";
-        for (int i = 0; i< retorno.tamanho;i++)
-        {
-
-            json = concatena (json,"{\"USU_ID\":\"");
-            json = concatena(json,retorno.list_campos[i].campos[0].valor);
-            json = concatena(json,"\",\"EMP_VALOR_TOTAL\":\"");
-            json = concatena(json,retorno.list_campos[i].campos[1].valor);
-            json = concatena(json,"\",\"EMP_VALOR_PAGO\":\"");
-            json = concatena(json,retorno.list_campos[i].campos[2].valor);
-            json = concatena(json,"\",\"EMP_NUM_PARCELAS\":\"");
-            json = concatena(json,retorno.list_campos[i].campos[3].valor);
-            json = concatena(json,"\",\"EMP_JURUS_MENSAL\":\"");
-            json = concatena(json,retorno.list_campos[i].campos[4].valor);
-            json = concatena(json,"\",\"EMP_DATA_INICIO\":\"");
-            json = concatena(json,retorno.list_campos[i].campos[5].valor);
-            json = concatena(json,"\",\"T.EMP_COBRADO\":\"");
-            json = concatena(json,retorno.list_campos[i].campos[6].valor);
-            json = concatena(json, "\"}");
-
-        }
-        json = concatena(json, "]");
-
-        return json;
-
+        char * retornoJson = convertJSON(retorno);
+        return retornoJson;
     }
     else
     {
